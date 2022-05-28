78ae236cf67198f9cf2a9e840c8f21b4045e0b60
yuda <yudajgim@gmail.com>
2022-05-28 21:47:40 +0900

    DalcommSchool init

---
+++ src/main/java/dcsc/mvc/DalcommSchoolApplication.java
@@ -0,0 +1,13 @@
+package dcsc.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalcommSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalcommSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dcsc/mvc/DalcommSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dcsc.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalcommSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
