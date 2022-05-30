b16765ad283e56f99070c21d155916a7adb74a70
sunjung <sunj20037@gmail.com>
2022-05-30 22:24:04 +0900

    Adjust/Coupon-Service,Repository

--- src/main/java/dcsc/mvc/service/adjust/AdjustService.java
+++ src/main/java/dcsc/mvc/service/adjust/AdjustService.java
@@ -1,32 +1,48 @@
 package dcsc.mvc.service.adjust;
 
+import java.util.List;
+
+import dcsc.mvc.domain.classes.Adjust;
+
 public interface AdjustService {
 	/**
 	 * 정산 가능 금액 조회 기능
-	 * @param int teacherId(정렬기준)
-	 * @return int 
+	 * @param String teacherId(검색기준)
+	 * @return int (정산금액)
 	 * */
+	int selectAdjust(String teacherId);
 	
 	
 	/**
 	 * 정산 신청 기능
-	 * @param Adjust(String teacherId, int adjustPrice, String bank, String depositor, String account)
+	 * @param Adjust(강사아이디,정산금액,은행명,예금주,계좌번호)
 	 * @return int(신청된 데이터 수)
+	 * 정산 가능 금액 변경 : 총 수익에서 정산 신청 금액 차감 (update)
 	 * */
+	void insertAdjust(Adjust adjust);
 	
 	
 	/**
 	 * 요청된 정산 처리 기능
+	 * 
+	 * 정산 상태 '진행중'으로 변경 (update)
+	 * 
+	 * state로 정산 상태 관리(정산테이블의 정산일,정산상태Id변경)
+	 * 
 	 * @param int adjustStateId(정렬기준)
 	 * @return int(수정된 데이터 수)
 	 * */
+	void updateAdjust(Long adjustId, String state);
 	
-	
+
 	/**
 	 * 정산 내역 조회 기능
-	 * @param String teacherId(정렬기준)
-	 * @return List<AdjustDTO>
+	 * @param String teacherId(검색기준)
+	 * @return List<Adjust>
 	 * */
-
+	List<Adjust> selectByTeacherId(String teacherId);
+	
+	
+	
 }
  
--- src/main/java/dcsc/mvc/service/coupon/CouponService.java
+++ src/main/java/dcsc/mvc/service/coupon/CouponService.java
@@ -1,67 +1,99 @@
 package dcsc.mvc.service.coupon;
 
+import java.util.List;
+
+import dcsc.mvc.domain.coupon.Coupon;
+import dcsc.mvc.domain.coupon.IssueCoupon;
+
 public interface CouponService {
 	/**
 	 * 자신이 보유한(발급 받은) 쿠폰 조회 기능; 학생
-	 * @param studentId (정렬 기준)
-	 * @return List<issueCoupon>
+	 * @param studentId (검색 기준)
+	 * @return List<IssueCoupon>
 	 * */
-	
+	List<IssueCoupon> selectByStudentId(String studentId);
 	
 	/**
 	 * 클래스 발급 쿠폰 조회 기능 ; 선생님
-	 * @param classId (정렬 기준)
+	 * @param classId (검색 기준)
 	 * @return List<Coupon>
 	 * */
+	List<Coupon> selectByClassId(String classId);
+	
+	
+	/**
+	 * 전체 발급 쿠폰 조회 기능 ; 선생님
+	 * @param teacherId (검색 기준)
+	 * @return List<Coupon>
+	 * */
+	List<Coupon> selectByTeacherId(String teacherId);
 	
 	
 	/**
 	 * 클래스 쿠폰 등록 기능 ; 선생님
 	 * @param Coupon(classId, couponName, couponDc, couponEndDate)
-	 * @return int(등록된 데이터 수)
+	 * @return void
 	 * */
+	void insertCoupon(Coupon coupon);
+	
 	
 	/**
 	 * 클래스 쿠폰 수정 기능 ; 선생님
-	 * @param Coupon(classId, couponName, couponDc, couponEndDate)
-	 * @return int(수정된 데이터 수)
+	 * @param Coupon(couponId, couponName, couponDc, couponEndDate)
+	 * @return void
 	 * */
-	
+	void updateCoupon(Coupon coupon);
 	
 	/**
 	 * 클래스 쿠폰 삭제 기능 ; 선생님
-	 * @param couponId
-	 * @return int(삭제된 데이터 수)
+	 * @param String couponId
+	 * @return void
 	 * */
+	void deleteCoupon(String couponId);
+	
+	/**
+	 * 클래스 쿠폰 상태 변경 기능 ; 선생님
+	 * @param Coupon coupon(couponId,couponStateId)
+	 * @return void
+	 * */
+	void updateCouponState(Coupon coupon);
+	
 	
 	/**
 	 * 등록한 전체 쿠폰(클래스 쿠폰 + 이벤트 쿠폰)조회 기능 ; 관리자
-	 * @param couponId(정렬기준)
+	 * @param 
 	 * @return List<Coupon>
 	 * */
+	List<Coupon> selectAll();
+	
 	
 	/**
 	 * 학생이 보유한 쿠폰 조회 기능 ; 관리자
-	 * @param studentId(정렬기준)
-	 * @return List<issueCoupon>
+	 * @param studentId(검색기준)
+	 * @return List<IssueCoupon>
 	 * */
+	List<IssueCoupon> selectByStudent(String studentId);
 	
 	/**
 	 * 이벤트 쿠폰 등록 기능 ; 관리자
-	 * @param Coupon(classId, couponName, couponDc, couponEndDate)
-	 * @return int(등록된 데이터 수)
+	 * @param Coupon(couponName, couponDc, couponEndDate)
+	 * @return void
 	 * */
+	void insertEventCoupon(Coupon coupon);
 	
 	/**
 	 * 이벤트 쿠폰 수정 기능 ; 관리자
-	 * @param Coupon(classId, couponName, couponDc, couponEndDate)
-	 * @return int(수정된 데이터 수)
+	 * @param Coupon(couponId, couponName, couponDc, couponEndDate)
+	 * @return void
 	 * */	
+	void updateEventCoupon(Coupon coupon);
+	
 	
 	/**
 	 * 이벤트 쿠폰 삭제 기능 ; 관리자
 	 * @param couponId
-	 * @return int(삭제된 데이터 수)
+	 * @return void
 	 * */
+	void deleteEventCoupon(String couponId);
 
 }
