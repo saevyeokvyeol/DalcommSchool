6a4bd05b6d006b175c62c4b812aa550203f43a1c
rmflsms <84006878+rmflsms@users.noreply.github.com>
2022-06-09 17:01:16 +0900

    FAQ 검색 및 카테고리 추가

--- src/main/java/dcsc/mvc/repository/board/FAQRepository.java
+++ src/main/java/dcsc/mvc/repository/board/FAQRepository.java
@@ -1,10 +1,18 @@
 package dcsc.mvc.repository.board;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import dcsc.mvc.domain.board.Faq;
+import dcsc.mvc.domain.board.Notice;
 
 public interface FAQRepository extends JpaRepository<Faq, Long>, QuerydslPredicateExecutor<Faq>{
-
+	
+	/**
+	 * 컬럼명에 따라 다른 메소드 호출
+	 * */
+	List<Faq> findByFaqTitleIsLike(String faqTitle);
+	List<Faq> findByFaqContentIsLike(String faqContent);
 }
